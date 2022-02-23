58750e94d99b5f6da4227a31e736f0485014942b
Abdulatip <example@gmail.com>
2022-02-23 14:05:54 +0300

    9commit

---
+++ src/js/validateform.js
@@ -0,0 +1,26 @@
+$(function() {  
+    function validateForms (form) {
+        $(form).validate({
+            rules: {
+                name: "required",
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                  }        
+            },
+            messages: {
+                name: "Пожалуйста введите свое имя",
+                phone: "Пожалуйста введите свой телефон",
+                email: {
+                  required: "Пожалуйста введите свою почту",
+                  email: "Неправильно введен адрес почты"
+                }
+            }
+        });
+    };
+    
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+});
